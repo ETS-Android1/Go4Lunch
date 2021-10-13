752e6313286941fecf30def1a3408ea5abfbbbd4
Frederic Thiery <frederic.thiery@gmail.com>
2021-10-13 11:48:31 +0200

    Enabled BottomNavigation and implemented MapBox location

--- app/src/main/java/com/fthiery/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/fthiery/go4lunch/ui/MainActivity.java
@@ -1,17 +1,22 @@
 package com.fthiery.go4lunch.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.fthiery.go4lunch.R;
 import com.fthiery.go4lunch.viewmodel.MyViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
@@ -44,18 +49,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.navigation_map_view, R.id.navigation_list_view, R.id.navigation_workmates_view)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(bottomNavView, navController);
 
-        navigationView.setNavigationItemSelectedListener(this);
+        binding.navView.setNavigationItemSelectedListener(this);
 
         if (!viewModel.isCurrentUserLogged()) {
             startSignInActivity();
@@ -96,12 +102,43 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+                showSnackBar(getString(R.string.connection_succeed));
+                recreate();
+            } else {
+                // ERRORS
+                if (response == null) {
+                    showSnackBar(getString(R.string.error_authentication_canceled));
+                } else if (response.getError()!= null) {
+                    if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK){
+                        showSnackBar(getString(R.string.error_no_internet));
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showSnackBar(getString(R.string.error_unknown_error));
+                    }
+                }
+                finish();
+            }
+        }
+    }
+
+    // Show Snack Bar with a message
+    private void showSnackBar( String message){
+        Snackbar.make(binding.getRoot(), message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.logout) {
             viewModel.signOut(this);
             binding.drawerLayout.closeDrawer(GravityCompat.START);
             startSignInActivity();
-            finish();
         }
         return false;
     }
--- app/src/main/java/com/fthiery/go4lunch/ui/map/MapFragment.java
+++ app/src/main/java/com/fthiery/go4lunch/ui/map/MapFragment.java
@@ -4,25 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.fthiery.go4lunch.R;
 import com.fthiery.go4lunch.databinding.FragmentMapBinding;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
-public class MapFragment extends Fragment {
+import java.util.List;
 
+public class MapFragment extends Fragment implements PermissionsListener, OnMapReadyCallback {
+
+    private PermissionsManager permissionsManager;
     private MapViewModel mapViewModel;
+    private MapboxMap mapboxMap;
     private FragmentMapBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -35,27 +44,121 @@ public class MapFragment extends Fragment {
         View root = binding.getRoot();
 
         binding.mapView.onCreate(savedInstanceState);
-        binding.mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        binding.mapView.getMapAsync(this);
 
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
+        return root;
+    }
 
-                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
 
-                    }
-                });
+        mapboxMap.setStyle(Style.MAPBOX_STREETS,
+                this::enableLocationComponent);
 
+        binding.locationFAB.setOnClickListener(view -> {
+            if (PermissionsManager.areLocationPermissionsGranted(this.getContext())) {
+                LocationComponent locationComponent = mapboxMap.getLocationComponent();
+                locationComponent.zoomWhileTracking(15);
+                locationComponent.setCameraMode(CameraMode.TRACKING_GPS_NORTH);
+            } else {
+                permissionsManager = new PermissionsManager(this);
+                permissionsManager.requestLocationPermissions(this.getActivity());
             }
         });
-        return root;
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this.requireContext())) {
+
+            // Get an instance of the component
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+            // Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this.requireContext(), loadedMapStyle).build());
+
+            // Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING_GPS_NORTH);
+
+            // Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this.getActivity());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this.getContext(), R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this.getContext(), R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            if (this.getActivity() != null) this.getActivity().finish();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        binding.mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        binding.mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        binding.mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        binding.mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        binding.mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        binding.mapView.onLowMemory();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        binding.mapView.onDestroy();
         binding = null;
     }
 }
